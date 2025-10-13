15cacd1d0b4bc6561ae5cca68c615438d95d3649
Antoine Caron-Guay <antoine.caron-guay@umontreal.ca>
2025-10-13 17:50:52 -0400

    adding new Tiputini dataset

--- Labelbox/2025_Tiputini/clean_tbs_splist.R
+++ Labelbox/2025_Tiputini/clean_tbs_splist.R
@@ -36,11 +36,11 @@ missing_species <- data.frame(
   Familia = c("Fabaceae", "Bignoniaceae", "Bignoniaceae", "Salicaceae",
               "Urticaceae", "Urticaceae", "Moraceae", "Fabaceae",
               "Cordiaceae", "Fabaceae", "Fabaceae", "Fabaceae",
-              "Euphorbiaceae"),
+              "Euphorbiaceae", "Dilleniaceae"),
   NombreActual = c("Albizia niopoides", "Tabebuia ochracea", "Tabebuia serratifolia", "Banara nitida",
                    "Cecropia putumayonis", "Cecropia latiloba", "Ficus insipida", "Inga microcoma",
                    "Cordia alliodora", "Stryphnodendron porcatum", "Inga velutina", "Lecointea peruviana",
-                   "Glycydendron amazonicum"),
+                   "Glycydendron amazonicum", "Tetracera volubilis"),
   stringsAsFactors = FALSE
 ) %>% 
   mutate(NombreMemoria = NombreActual)
@@ -161,3 +161,124 @@ labelbox_tbs_checklist <- bind_rows(yasuni_splist_withgbif,
 
 write.csv(labelbox_tbs_checklist, "data/labelbox_tbs_completelist.csv",
           fileEncoding = 'latin1', row.names = F, quote = T)
+
+
+# Adding new species list ------
+amazonia_species_path <- "data/Arboles_de_la_Amazonia_Ecuatoriana.csv"
+
+
+amazonia_species <- read.csv(amazonia_species_path, blank.lines.skip = TRUE)
+
+
+amazonia_species_cleaned <- amazonia_species %>% 
+  mutate(Family = str_to_title(Family)) %>% 
+  mutate(name = paste(ScientificName, ScientificNameAuthorship, sep=" "))
+
+amazonia_species_gbif <- name_backbone_checklist(amazonia_species_cleaned)
+not_exact <- amazonia_species_gbif %>% filter(matchType != "EXACT")
+
+amazonia_species_gbif <- amazonia_species_gbif %>% 
+  filter(rank == "SPECIES") %>%
+  mutate(gbif_accepted_scientific_name = species,
+         gbif_accepted_taxon_id = speciesKey)
+
+# Find taxon IDs in amazonia_species_gbif not present in yasuni_splist_withgbif
+missing_taxon_ids <- setdiff(amazonia_species_gbif$gbif_accepted_taxon_id, yasuni_splist_withgbif$gbif_accepted_taxon_id)
+
+amazonia_species_filtered <- amazonia_species_gbif %>%
+  distinct(gbif_accepted_taxon_id, .keep_all = TRUE) %>% 
+  filter(gbif_accepted_taxon_id %in% missing_taxon_ids)
+
+amazonia_species_duplicate <- amazonia_species_gbif %>% 
+  group_by(gbif_accepted_taxon_id) %>% 
+  filter(n() > 1) %>%
+  ungroup()
+
+# Species ------
+amazonia_splist_withgbif <- amazonia_species_filtered %>%
+  select(family, gbif_accepted_scientific_name, gbif_accepted_taxon_id)
+
+# Families ------
+amazonia_familieslist <- bind_rows(amazonia_species_cleaned %>%
+                            rename(family= Family) %>%
+                            select(family),
+                          amazonia_species_gbif %>%
+                            select(family),
+                          data.frame(family = c("Fabaceae-Mimosoideae", 
+                                                "Fabaceae-Papilionoideae", 
+                                                "Fabaceae-Caesalpiniodeae"))) %>%
+  filter(!is.na(family),
+         family != "") %>%
+  distinct() %>% 
+  mutate(name = family) %>% 
+  mutate(name = case_when(
+    str_detect(name, "Fabaceae") ~ "Fabaceae",
+    TRUE ~ name
+  ))
+
+
+amazonia_familieslist_gbif <- name_backbone_checklist(amazonia_familieslist, kingdom = 'Plantae')
+amazonia_familieslist_withgbif <- amazonia_familieslist_gbif %>% 
+  filter(matchType == "EXACT") %>%
+  select(taxon_code = verbatim_family,
+         gbif_accepted_scientific_name = family,
+         gbif_accepted_taxon_id = familyKey) %>% 
+  arrange(gbif_accepted_scientific_name)
+
+missing_families_ids <- setdiff(amazonia_familieslist_withgbif$gbif_accepted_taxon_id, familieslist_withgbif$gbif_accepted_taxon_id)
+
+amazonia_familieslist_filtered <- amazonia_familieslist_withgbif %>%
+  filter(gbif_accepted_taxon_id %in% missing_families_ids)
+
+# Genera ------
+amazonia_generalist <- bind_rows(amazonia_species_cleaned %>%
+                          mutate(genus = word(ScientificName, 1)) %>%
+                          select(genus),
+                        amazonia_species_gbif %>%
+                          select(genus)) %>%
+  distinct()
+
+amazonia_generalist_gbif <- name_backbone_checklist(amazonia_generalist, phylum = 'Tracheophyta')
+
+#Fix not exact matches
+amazonia_generalist_fixed <- amazonia_generalist %>%
+  mutate(name = paste(genus),
+         name = case_when(
+           name == "Albizia" ~ "Albizia Durazz.",
+           name == "Heisteria" ~ "Heisteria Jacq.",
+           name == "Hirtella" ~ "Hirtella L.",
+           name == "Hura" ~ " Hura L.",
+           name == "Lunania" ~ "Lunania Hook.",
+           name == "Simira" ~ "Simira Aubl.",
+           TRUE ~ name
+         )) %>%
+  select(name)
+
+amazonia_generalist_gbif <- name_backbone_checklist(amazonia_generalist_fixed, phylum = 'Tracheophyta')
+not_exact <- amazonia_generalist_gbif %>% filter(matchType != "EXACT")
+
+amazonia_generalist_withgbif <- amazonia_generalist_gbif %>%
+  filter(rank == "GENUS") %>% 
+  mutate(gbif_accepted_scientific_name = genus,
+         gbif_accepted_taxon_id = genusKey) %>%
+  select(family, gbif_accepted_scientific_name, gbif_accepted_taxon_id)
+
+missing_genera_ids <- setdiff(amazonia_generalist_withgbif$gbif_accepted_taxon_id, generalist_withgbif$gbif_accepted_taxon_id)
+
+amazonia_generalist_filtered <- amazonia_generalist_withgbif %>%
+  filter(gbif_accepted_taxon_id %in% missing_genera_ids)
+
+# Complete checklist for Labelbox ------
+labelbox_tbs_checklist_adding <- bind_rows(amazonia_splist_withgbif,
+                                    amazonia_familieslist_filtered,
+                                    amazonia_generalist_filtered) %>% 
+  select(gbif_accepted_taxon_id, gbif_accepted_scientific_name) %>% 
+  arrange(gbif_accepted_scientific_name)
+
+write.csv(labelbox_tbs_checklist_adding, "data/labelbox_tbs_completelist_Amazonia_Ecuatoriana.csv",
+          fileEncoding = 'latin1', row.names = F, quote = T)
+
+
+
+
+
